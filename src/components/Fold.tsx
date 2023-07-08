42162dc0e68a455ec5484bc1b961777883acdbba
oldpilgrim <40671177+oldpilgrim@users.noreply.github.com>
2023-07-09 00:22:31 +0100

    add some text to Fold.tsx

--- src/components/Fold.tsx
+++ src/components/Fold.tsx
@@ -1,4 +1,4 @@
-import {Box} from "@mui/material";
+import {Box, Container, Typography} from "@mui/material";
 
 // https://unsplash.com/t/3d-renders
 
@@ -13,7 +13,38 @@ export default function Fold() {
       backgroundPosition: "center",
       backgroundSize: "cover",
       backgroundRepeat: "no-repeat",
-      backgroundColor: "black"
+      backgroundColor: "black",
+
+      // lazy center
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+
+      color: "white"
     }}
-  />;
+  >
+    <Container maxWidth={"sm"}>
+      <Typography sx={{fontWeight: "bold"}} paragraph>
+        Games used to be made by nerds. Now they are made by business people.
+      </Typography>
+      <Typography paragraph>
+        The games industry lacks innovation.
+        The reason is simple - it's risky to bet big budgets on untested ideas.
+        So developers stick to what's worked before, churning out sequel after sequel with formulaic gameplay.
+        {/*7 of 2022's 10 most-played game franchises also featured in 2021's top 10.*/}
+      </Typography>
+      <Typography paragraph>
+        The catch is that to make great games that sell, developers need to push boundaries.
+        The most successful games are made by the most creative teams,
+        unshackled from the diktats of management.
+      </Typography>
+      <Typography paragraph>
+        Greyhame Interactive is a bootstrapped micro game studio based in London and Hong Kong.
+      </Typography>
+    </Container>
+    <Typography sx={{fontWeight: "bold", marginTop: 4}}>
+      © {new Date().getFullYear()} Greyhame Interactive
+    </Typography>
+  </Box>;
 }
