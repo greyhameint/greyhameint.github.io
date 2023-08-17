196a480074daa339a15abe0ff2b88da1700bba65
oldpilgrim <40671177+oldpilgrim@users.noreply.github.com>
2023-08-17 20:26:11 +0100

    change to soweird union

--- src/components/Fold.tsx
+++ src/components/Fold.tsx
@@ -9,7 +9,7 @@ export default function Fold() {
       // width: "100vw",
       height: "100vh",
       // https://unsplash.com/photos/pELem6Qj_GE
-      backgroundImage: "url(https://images.unsplash.com/photo-1680288103814-114a5052ae9d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80)",
+      backgroundImage: "url(https://images.unsplash.com/photo-1683560901896-52ed09af616f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2232&q=80)",
       backgroundPosition: "center",
       backgroundSize: "cover",
       backgroundRepeat: "no-repeat",
@@ -24,27 +24,13 @@ export default function Fold() {
       color: "white"
     }}
   >
-    <Container maxWidth={"sm"}>
-      <Typography sx={{fontWeight: "bold"}} paragraph>
-        Games used to be made by nerds. Now they are made by business people.
-      </Typography>
+    <Container maxWidth={"sm"} sx={{backdropFilter: "blur(10px)", padding: 4}}>
       <Typography paragraph>
-        The games industry lacks innovation.
-        The reason is simple - it's risky to bet big budgets on untested ideas.
-        So developers stick to what's worked before, churning out sequel after sequel with formulaic gameplay.
-        {/*7 of 2022's 10 most-played game franchises also featured in 2021's top 10.*/}
+        Soweird Union is a bootstrapped micro game studio based in London and Hong Kong.
       </Typography>
-      <Typography paragraph>
-        The catch is that to make great games that sell, developers need to push boundaries.
-        The most successful games are made by the most creative teams,
-        unshackled from the diktats of management.
-      </Typography>
-      <Typography paragraph>
-        Greyhame Interactive is a bootstrapped micro game studio based in London and Hong Kong.
+      <Typography sx={{fontWeight: "bold", marginTop: 4}}>
+        © {new Date().getFullYear()} Soweird Union
       </Typography>
     </Container>
-    <Typography sx={{fontWeight: "bold", marginTop: 4}}>
-      © {new Date().getFullYear()} Greyhame Interactive
-    </Typography>
   </Box>;
 }
--- src/components/Masthead.tsx
+++ src/components/Masthead.tsx
@@ -1,21 +1,21 @@
 import {AppBar, SvgIcon, Toolbar, Typography} from "@mui/material";
+import HammerSickle from "./HammerSickle";
 
-function Triangle() {
+function Logo() {
   return (
-    <SvgIcon fontSize={"small"} sx={{position: "relative", top: 2}}>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 490">
-        <path fill="white" d="M0 456.701h490L245 33.299 0 456.701zm137.318-77.197L245 193.409l107.682 186.094H137.318z"/>
-      </svg>
+    <SvgIcon fontSize={"large"}>
+      <HammerSickle/>
     </SvgIcon>
   );
 }
 
 export default function Masthead() {
   return (
-    <AppBar position="fixed" elevation={0} color={"transparent"}>
+    <AppBar position="fixed" elevation={0} color={"transparent"} sx={{backdropFilter: "blur(10px)"}}>
       <Toolbar>
-        <Typography variant="h5" sx={{flexGrow: 1, fontWeight: "bold"}} color="white">
-          G R E Y H <Triangle/> M E
+        <Logo/>
+        <Typography variant="h5" sx={{flexGrow: 1, fontWeight: "bold", ml: 1}} color="white">
+          Soweird Union
         </Typography>
       </Toolbar>
     </AppBar>
